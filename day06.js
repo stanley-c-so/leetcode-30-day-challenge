e54bc49ba903484911cf78b14b33d8b801b85a6a
stanley-c-so <stanley.c.so@gmail.com>
2020-04-07 20:14:22 -0400

    added day07

---
+++ day06.js
@@ -0,0 +1,76 @@
+// --- Day 6: Group Anagrams ---
+
+// Given an array of strings, group anagrams together.
+
+// Example:
+
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+// Note:
+
+// All inputs will be in lowercase.
+// The order of your output does not matter.
+
+// ----------
+
+function solution_1 (strs) {
+  const obj = {};
+  for (const str of strs) {
+    const sorted = str.split('').sort().join('');
+    if (!(sorted in obj)) obj[sorted] = [];
+    obj[sorted].push(str);
+  }
+  return Object.values(obj)
+    .map(subarray => subarray.sort())       // this sort is just for my testing suite - not needed for leetcode
+  ;
+}
+
+function solution_2 (strs) {
+  const lettersToNumbers = {};
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 26; i++) {
+    lettersToNumbers[alphabet[i]] = i;                // {a: 0, b: 1, ..., z: 25}
+  }
+  const obj = {};
+  for (const str of strs) {
+    const freq = Array(26).fill(0);                   // [0, 0, 0, ... , 0]
+    for (const c of str) {
+      freq[lettersToNumbers[c]]++;                    // e.g. 'ABC' --> [1, 1, 1, 0, 0, ... , 0]
+    }
+    const serial = freq.join(',');                    // e.g. 'ABC' --> '1,1,1,0,0,...,0'
+    if (!(serial in obj)) obj[serial] = [];
+    obj[serial].push(str);
+  }
+  return Object.values(obj)
+    .map(subarray => subarray.sort())       // this sort is just for my testing suite - not needed for leetcode
+  ;
+}
+
+const groupAnagrams = solution_2;
+
+// TEST CASES
+
+const test = require('./_test');
+const testNum = [1];
+let input, expected;
+const func = groupAnagrams;
+const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
+const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
+const lowestTest = 0 || 0;
+const highestTest = 0 || Infinity;
+
+// Test case 1
+input = {
+  strs: ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'],
+};
+expected = [
+  ['ate', 'eat', 'tea'],
+  ['nat','tan'],
+  ['bat'],
+];
+test(sortedFunc, input, expected.sort(), testNum, lowestTest, highestTest);
---
+++ day07.js
@@ -0,0 +1,94 @@
+// --- Day 7: Counting Elements ---
+
+// Given an integer array arr, count element x such that x + 1 is also in arr.
+
+// If there're duplicates in arr, count them seperately.
+
+// Example 1:
+
+// Input: arr = [1, 2, 3]
+// Output: 2
+// Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+
+// Example 2:
+
+// Input: arr = [1, 1, 3, 3, 5, 5, 7, 7]
+// Output: 0
+// Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+
+// Example 3:
+
+// Input: arr = [1, 3, 2, 3, 5, 0]
+// Output: 3
+// Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+
+// Example 4:
+
+// Input: arr = [1, 1, 2, 2]
+// Output: 2
+// Explanation: Two 1s are counted cause 2 is in arr.
+
+// Constraints:
+
+// 1 <= arr.length <= 1000
+// 0 <= arr[i] <= 1000
+
+// ----------
+
+function solution_1 (arr) {
+  const dict = {};
+  for (const num of arr) {
+    if (!(num in dict)) dict[num] = 0;
+    dict[num]++;
+  }
+  let count = 0;
+  for (const num of arr) {
+    if (num + 1 in dict) count++;
+  }
+  return count;
+}
+
+// one-liner - in the ternary, we can say `d[n] ?` instead of `(n in d) ?` because all existing dictionary entries will be truthy (non-zero)
+
+var solution_2=(a,d={},c=0)=>(a.map(n=>d[n]=d[n]?d[n]+1:1),a.map(n=>d[n+1]?c++:0))&&c
+
+const countElements = solution_1;
+
+// TEST CASES
+
+const test = require('./_test');
+const testNum = [1];
+let input, expected;
+const func = countElements;
+const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
+const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
+const lowestTest = 0 || 0;
+const highestTest = 0 || Infinity;
+
+// Test case 1
+input = {
+  arr: [1, 2, 3],
+};
+expected = 2;
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 2
+input = {
+  arr: [1, 1, 3, 3, 5, 5, 7, 7],
+};
+expected = 0;
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 3
+input = {
+  arr: [1, 3, 2, 3, 5, 0],
+};
+expected = 3;
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 4
+input = {
+  arr: [1, 1, 2, 2],
+};
+expected = 2;
+test(func, input, expected, testNum, lowestTest, highestTest);
