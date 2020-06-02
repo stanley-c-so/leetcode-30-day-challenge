15590517437a5737f95e59846e8a4eb0972fd2ad
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-06-02 11:51:34 -0400

    added missing problem description

--- day09.js
+++ day09.js
@@ -1,6 +1,8 @@
 // --- Day 9: Backspace String Compare ---
 
-//
+// Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+// Note that after backspacing an empty text, the text will continue empty.
 
 // ----------
 
